f6ebf4f1272555d5dad4859a08777a1974b33603
ACristoff <acristoffanini@gmail.com>
2021-09-20 20:58:59 +0000

    refactored tail.js

--- tail.js
+++ tail.js
@@ -1,17 +1,3 @@
-// ASSERT EQUAL FUNCTION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// // TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual("Alex", "Alex");
-// assertEqual(1, 1);
-// assertEqual(1, 2);
 
 //TAIL FUNCTION
 const tail = function(arrayArg) {
@@ -19,18 +5,4 @@ const tail = function(arrayArg) {
   return newTail;
 };
 
-//TEST CODE
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(words).length, 3);
-console.log(tail(words));
-const words2 = [1];
-console.log(tail(words2));
-
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,6 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
+const tail = require('../tail')
 
 //HEAD TEST CODE
 assertEqual(head([5,6,7]), 5);
@@ -11,3 +12,6 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual("Alex", "Alex");
 assertEqual(1, 1);
 assertEqual(1, 2);
+
+const example = ['hello', 'Alex']
+assertEqual(tail(example).length, 1);
